35f3e9bc6ea43a5b1b844164e0db0d8074d4115e
PashMan <84240835+PashMan@users.noreply.github.com>
2026-03-11 19:35:31 +0300

    Delete test-git.js

--- test-git.js
+++
@@ -1,7 +0,0 @@
-import { execSync } from 'child_process';
-try {
-  const log = execSync('git log -p -2 services/kodik.ts').toString();
-  console.log(log.substring(0, 2000));
-} catch (e) {
-  console.error(e.message);
-}
