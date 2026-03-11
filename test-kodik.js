642229b6a756878e56c0cd6878f8e1ccf3021531
PashMan <84240835+PashMan@users.noreply.github.com>
2026-03-11 19:22:50 +0300

    Delete test-kodik.js

--- test-kodik.js
+++
@@ -1,6 +0,0 @@
-async function run() {
-  const res = await fetch('https://kodikapi.com/search?token=b3b563060d02ee000ca18740b7842ca0&shikimori_id=16498,1535');
-  const data = await res.json();
-  console.log(data);
-}
-run();
